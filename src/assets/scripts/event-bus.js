573a3805304a0f88c69179708f9bf53e14ba2172
mr150 <im150@bk.ru>
2025-12-29 03:34:07 +0300

    fix: fix event bus logic

--- src/assets/scripts/event-bus.js
+++ src/assets/scripts/event-bus.js
@@ -1,26 +1,27 @@
 export class EventBus {
-	constructor() {
-		this.events = {}
+	events = {};
+
+	on(event, handler) {
+		if (this.events[event] == undefined) {
+			this.events[event] = [];
+		}
+
+		this.events[event].push(handler);
 	}
 
-	on(event, handler){
-		if (!this.events[event]){
-			this.events[event] = []
+	off(event, handler) {
+		if (this.events[event] == undefined) {
+			return;
 		}
-		this.events[event].push(handler)
+
+		this.events[event] = this.events[event].filter((h) => h !== handler);
 	}
 
-	off(event, handler){
-		if (!this.events[event]){
-			return
+	emit(event, data) {
+		if (this.events[event] == undefined) {
+			throw new Error('There is no such event!');
 		}
-		this.events[event].filter(h => h !== handler);
-	}
 
-	emit(event, data){
-		if (!this.events[event]){
-			throw new Error("There is no such event!")
-		}
-		this.events[event].forEach(handler => handler(data))
+		this.events[event].forEach((handler) => handler(data));
 	}
 }
