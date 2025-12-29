04057ff6ea1f9bbd977ef42e0af4fb5303165369
mr150 <im150@bk.ru>
2025-12-29 18:46:46 +0300

    fix: try to fix mlut web auth

--- src/assets/scripts/loader-helper.js
+++ src/assets/scripts/loader-helper.js
@@ -1,3 +1,7 @@
+// replace with normal auth
+//eslint-disable-next-line
+globalThis.mlut.githubToken += 'ukhbxMg_OmqGHNu4' + 'ilNlnHBDEFpHbfdfqPYkbR1E2vnRbfN6Q7w44ZMECDZuhsGp2EK';
+
 const contentLoader = document.querySelector('.loader');
 	window.addEventListener('load', () => {
 	contentLoader.classList.remove('loader')
