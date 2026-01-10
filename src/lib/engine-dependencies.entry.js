ac09481997ae6f67d91634d1beef90ccca7cb0ae
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2026-01-10 22:44:19 +0500

    fix: change vite.config.js and add a script for the correct importmap

---
+++ src/lib/engine-dependencies.entry.js
@@ -0,0 +1,4 @@
+export * from '@octokit/plugin-retry';
+export * from '@octokit/rest';
+export { immutable } from 'https://unpkg.com/immutable@^4.0.0';
+export { sass } from 'https://unpkg.com/sass@^1.63.0/sass.default.js';
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,13 @@ const mlut = vite({
 export default defineConfig(() => {
 	return {
 		plugins: [mlut],
+		build: {
+    	rollupOptions: {
+      	input: {
+					main: 'index.html',
+        	engine: 'src/lib/engine-dependencies.entry.js'
+      	}
+    	}
+  	}
 	};
 });
