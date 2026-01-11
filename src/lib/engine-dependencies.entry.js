618ab612c86d6a7bf4a733e6c042b68ed846fa49
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2026-01-11 20:04:42 +0500

    fix: roll back the importmap updates

--- src/lib/engine-dependencies.entry.js
+++
@@ -1,4 +0,0 @@
-export * from '@octokit/plugin-retry';
-export * from '@octokit/rest';
-export { immutable } from 'https://unpkg.com/immutable@^4.0.0';
-export { sass } from 'https://unpkg.com/sass@^1.63.0/sass.default.js';
--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,5 @@ const mlut = vite({
 export default defineConfig(() => {
 	return {
 		plugins: [mlut],
-		build: {
-    	rollupOptions: {
-      	input: {
-					main: 'index.html',
-        	engine: 'src/lib/engine-dependencies.entry.js'
-      	}
-    	}
-  	}
 	};
 });
