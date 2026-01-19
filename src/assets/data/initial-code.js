cee631b28d7e07e4f1248df2346cff96ef785128
Ramazan Maksyutov <ramazanmaksyutov@outlook.com>
2026-01-19 17:17:34 +0500

    fix: change the default initial code

--- src/assets/data/initial-code.js
+++ src/assets/data/initial-code.js
@@ -1,17 +1,23 @@
 export const initialLayout = `
 <div class="-Gdl120d,$core750,$core650,$core600 Fnf-mUss H100vh P5u;0 D-f Jc-c Ai-c">
-	<div class="D-f Jc-c Ai-c Fld-c wrapper">
-		<img class="D Mxw480 M0;a;5u md_W100p" alt="mlut logo" src="/img/mlut.png">
-		<h1 class="Txa-c C-$accent900 Fns8u Lnh1.1 M0;0;5u P0;1u md_Fns10u">
-			Make CSS exciting again!
-		</h1>
-		<p class="C-$accent850 M0;0;5u P0;2u Mxw750 Txa-c Fns4.4u md_Fns5.2u">
-			Atomic CSS toolkit with Sass and ergonomics for creating styles of any complexity
-		</p>
-		<button class="btn Mnw18gg P2u;3u Bdrd1u Bgc-$brand Bgc-$brand500_h C#fff">
-			Getting started
-		</button>
-	</div>
+  <div class="D-f Jc-c Ai-c Fld-c wrapper">
+    <img class="D Mxw480 W60p M0;a;15u md_W100p" alt="mlut logo" src="/img/mlut.png">
+    <div class="Ps W15p Apcr1 Tf -Rt45d Mb10u">
+      <div class="-Sz100p -Gdl-45d,#b70000,red;30p,red;80p,#b70000"></div>
+      <div class="-Sz100p Bgc-red Ps-a T-50p Bdrd100p -Gdl-90d,red,red;80p,#b70000"></div>
+      <div class="-Sz100p Bgc-red Ps-a L-50p T0 Bdrd100p -Gdl-90d,red,red;80p,#b70000">
+      </div>
+    </div>
+    <h1 class="Txa-c C-$accent900 Fns8u Lnh1.1 M0;0;5u P0;1u md_Fns10u">
+      Creative coding!
+    </h1>
+    <p class="C-$accent900 M0;0;10u Fns5u">
+      Yes, you can create pure CSS art using utility classes!
+    </p>
+    <button class="btn Mnw18gg P2u;3u Bdrd1u Bgc-$brand Bgc-$brand500_h C#fff">
+  	  Getting started
+	</button>
+  </div>
 </div>
 `;
 export const initialConfig = `
@@ -19,13 +25,11 @@ export const initialConfig = `
 
 :root {
 	--ml-brand:#f0438c;
-	--ml-brand500:rgb(233,30,99);
 }
 
 @media (prefers-color-scheme: dark){
   :root{
     --ml-accent900:#fff;
-    --ml-accent850:rgb(209,213,219,1);
     --ml-core750:#121828;
     --ml-core650:#251942;
     --ml-core600:#321933;
@@ -35,7 +39,6 @@ export const initialConfig = `
 @media (prefers-color-scheme: light){
   :root{
     --ml-accent900:rgb(17,24,39);
-    --ml-accent850:rgb(41,42,51,1);
     --ml-core750:rgb(236,240,246,0.5);
     --ml-core650:rgba(218,178,255,0.1);
     --ml-core600:rgba(142,197,255,0.4);
