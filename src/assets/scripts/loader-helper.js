e06e31d34d4753896ecb5f87bc33e4296a165348
mr150 <im150@bk.ru>
2026-01-28 22:42:08 +0300

    fix: update mlut web auth
    🤡

--- src/assets/scripts/loader-helper.js
+++ src/assets/scripts/loader-helper.js
@@ -1,6 +1,6 @@
 //replace with normal auth
 //eslint-disable-next-line
-globalThis.mlut.githubToken += 'ukhbxMg_OmqGHNu4' + 'ilNlnHBDEFpHbfdfqPYkbR1E2vnRbfN6Q7w44ZMECDZuhsGp2EK';
+globalThis.mlut.githubToken += 'pHJX2lKUGh5M_B9TrwNMoCtdEHMP70VlVH2lTkxVZ8WLpm1zp' + 'MAhiZ5cS2FXBO2LQD6IXFtL';
 
 const contentLoader = document.querySelector('.loader');
 window.addEventListener('load', () => {
